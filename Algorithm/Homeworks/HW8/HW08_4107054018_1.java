aba24ca49903594f02e427fabf2641e83aa6ea0c
Charlestocode <80250573+Charlestocode@users.noreply.github.com>
2022-06-20 19:50:57 +0800

    Add files via upload

---
+++ Algorithm/Homeworks/HW8/HW08_4107054018_1.java
@@ -0,0 +1,31 @@
+public class HW08_4107054018_1 extends Buy_Phone_v2 {
+    
+    @Override
+    public int[][] bestPhone(int[][] inputArr) {
+
+        int[][] temp = new int[3][6];
+        return temp;
+    }
+    
+
+
+
+
+    public static void main(String[] args) {
+
+        HW08_4107054018_1 test = new HW08_4107054018_1();
+        int[][] arrays = new int[3][6];
+
+        System.out.println(arrays[1]);
+    }
+}
+
+class ArrayList <T> {
+
+    private T[] list;
+    private int size = 0;
+
+    ArrayList() {
+        list = (T[]) new Object[8];
+    }
+}
